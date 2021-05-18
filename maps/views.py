e6fdf2bba84ee738ecfd5ff948b11f01cc2ebc33
antonioarf <antonioarf@al.insper.edu.br>
2021-05-17 23:27:26 -0300

    fff

--- maps/views.py
+++ maps/views.py
@@ -39,8 +39,11 @@ def api_pais(request):
                                 disponibilidade_quarentena =new_note_data['disponibilidade_quarentena'], 
                                 # nome_user = new_note_data['resp_4'],
                                 idade = new_note_data['idade'])
-
-        lista_paises = new_note_data['continente'][1:-1].split(",")
+        try:
+            lista_paises = new_note_data['continente'][1:-1].split(",")
+        except:
+            lista_paises = ["Brazil"]
+            
         idade = float(new_note_data['idade'])
         porcentagem = float(new_note_data['vac_pais'])
         
@@ -102,3 +105,44 @@ def api_feed(request):
             del final[1][e]
 
         return Response(final)
+
+@api_view(['GET', 'POST'])
+def api_mapa(request):
+    if request.method == 'POST':
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        new_note_data = request.data #''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+        pais = new_note_data['pais']
+        idade = float(new_note_data['idade'])
+        porcentagem = float(new_note_data['vac_pais'])
+        
+        try:
+            url = "https://covid-19-data.p.rapidapi.com/country"
+            querystring = {"name":pais}
+            headers = {'x-rapidapi-key': "af765fe219msha36798338e049f0p1d1272jsn67587def0601",'x-rapidapi-host': "covid-19-data.p.rapidapi.com" }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+
+            data =  pd.read_csv("https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/country_data/{}.csv".format(pais))
+            linha = data.tail(1)
+            pop_vacinada = linha['total_vaccinations'].values[0]
+            tempo_vacinando = data.shape[0]/30
+
+            url = "https://countriesnow.space/api/v0.1/countries/population"
+            response2 = requests.request("POST", url, headers={}, data={"country": pais})
+            pop_atual = response2.json()["data"]["populationCounts"][-1]['value']
+            
+            
+            delta = 0
+            if new_note_data['vac_propria']== 'True':
+                if idade < 60:
+                    delta += ((60 - idade)/(10)) **2 #meses
+            porcent_ja_vac = (pop_vacinada/pop_atual)*100
+            if porcentagem  > porcent_ja_vac:
+                dif = porcentagem - porcent_ja_vac
+                delta += tempo_vacinando*(dif/porcent_ja_vac)
+
+            resp = {"pais" : pais,"delta" : delta,"porcentagem": porcent_ja_vac} #, "idade": idade, "pop_vacinada": pop_vacinada, "pop_atual":pop_atual, "razao":porcent_ja_vac }
+        except Exception as e: 
+            print(e)
+            resp = ""
+            delta = 0
+        return Response(delta)
